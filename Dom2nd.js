e65f09bff4e9304308416972d1ea7160f2bd8a4e
VaibhaVnagre <nagrevaibhav001@gmail.com>
2023-01-20 23:57:18 +0530

    boomWlaaFile

---
+++ Dom2nd.js
@@ -0,0 +1,6 @@
+const BGcolor = document.getElementsByClassName("list-group-item");
+BGcolor[2].style.backgroundColor="green";
+
+for(var i=0; i<BGcolor.length; i++){
+    BGcolor[i].style.fontWeight = "bold";
+}
