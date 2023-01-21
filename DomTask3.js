e4c5f2b4bf24fee69f3c11232f1109f00b02cec5
VaibhaVnagre <nagrevaibhav001@gmail.com>
2023-01-21 11:07:08 +0530

    elementByTag.html

---
+++ DomTask3.js
@@ -0,0 +1,9 @@
+// const add_li = document.getElementsByClassName("list-group-item");
+// for(var i=0; i<add_li.length; i++){
+//     add_li[i].style.backgroundColor="orange";
+// }
+
+const add_li = document.getElementsByTagName("li");
+for(var i=0; i<add_li.length; i++){
+    add_li[i].style.backgroundColor="silver";
+}
